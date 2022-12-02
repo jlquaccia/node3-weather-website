c5c4ac9b9dd51f68f8174196afeeb21871a381df
jlquaccia <jlquaccia@gmail.com>
2022-12-02 11:04:06 -0800

    updated forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
       console.log(body.error);
       callback('Unable to find location.', undefined);
     } else {
-      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out and it feels like ${body.current.feelslike} degrees.`);
+      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out and it feels like ${body.current.feelslike} degrees. The humidity is ${body.current.humidity}.`);
     }
   });
 };
